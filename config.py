4c57d08a1e9bec336b9daee9da9dd1d428ba437e
Hamlet3000 <75612866+Hamlet3000@users.noreply.github.com>
2021-04-13 07:35:08 +0200

    Add files via upload
    initial upload

---
+++ config.py
@@ -0,0 +1,14 @@
+api_weather = "YOUR_WEATHER_API_KEY"             # The api Key from openweathermap.org
+loc_city = "Munich"                              # Enter your city name (this is for the weather)
+loc_region = "Germany"                           # Enter either your state or country
+weather_location = "Munich"
+news_count = 2                                   # How many news stories Vector should read (max is 7 to 9)
+vector_mood = "normal"                           # Normal is the only mood at the moment
+temperature = "metric"                           # Enter "imperial" or "metric"
+chattiness = 6                                   # Enter a number between "1" and "10". The number "5" is average. ("10" is VERY chatty.) This feature still needs work.
+sound_volume = 1                                 # Vector's sound effects between "1" and "5". (5 is very loud)
+voice_volume = 2                                 # Vector's voice volume between "1" and "5". (5 is very loud)
+news_feed = "YOUR_NEWS_FEED"                     # You can use any rss feed for this
+mail_imap = "imap.gmail.com"                     # or use some other imap server
+mail_account = "YOUR_E_MAIL"                     # the email address that is checked
+mail_pw = "YOUR_E_MAIL_PASSWORD"                 # the mail password
